1bf005edbef79ed517dc83c4667fc9be07833734
aswanabidin <aswanabidin@ymail.com>
2018-07-26 12:04:27 +0700

    card layout source and news

--- app/src/main/java/mumtaazstudio/newsreader_kotlin/Common/Common.kt
+++ app/src/main/java/mumtaazstudio/newsreader_kotlin/Common/Common.kt
@@ -9,4 +9,13 @@ object Common{
 
     val newsService:NewsServices
     get() = RetrofitClient.getClient(BASE_URL).create(NewsServices::class.java )
+
+    fun getNewsAPI(source:String) : String {
+        val apiUrl = StringBuilder("https://newsapi.org/v2/top-headlines?sources=")
+                .append(source)
+                .append("&apiKey=")
+                .append(API_KEY)
+                .toString()
+        return apiUrl
+    }
 }
